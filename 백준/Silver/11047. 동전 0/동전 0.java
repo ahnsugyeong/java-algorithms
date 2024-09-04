9845c8d154ce29e9873d111a43e13b1da1310a5f
Ahn Su-gyeong <ahnsugyeong@gmail.com>
2024-09-05 00:05:40 +0900

    [Silver IV] Title: 동전 0, Time: 100 ms, Memory: 14316 KB -BaekjoonHub

---
+++ 백준/Silver/11047. 동전 0/동전 0.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+
+        int[] coins = new int[N];
+        for (int i = 0; i < N; i++) {
+            coins[i] = Integer.parseInt(br.readLine());
+        }
+
+        Arrays.sort(coins);
+
+        int cnt = 0;
+        for (int i = N - 1; i >= 0; i--) {
+            int tmp = K / coins[i];
+            if (tmp > 0) {
+                cnt += tmp;
+                K -= tmp * coins[i];
+            }
+        }
+        System.out.println(cnt);
+    }
+}
