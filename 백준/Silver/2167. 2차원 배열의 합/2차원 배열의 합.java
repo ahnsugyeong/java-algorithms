7b248029d4d6e2d8e594f62f129b44b366257413
Ahn Su-gyeong <ahnsugyeong@gmail.com>
2024-10-14 23:58:52 +0900

    [Silver V] Title: 2차원 배열의 합, Time: 1936 ms, Memory: 49712 KB -BaekjoonHub

---
+++ 백준/Silver/2167. 2차원 배열의 합/2차원 배열의 합.java
@@ -0,0 +1,37 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        int[][] board = new int[N + 1][M + 1];
+        for (int i = 1; i <= N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 1; j <= M; j++) {
+                board[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        int K = Integer.parseInt(br.readLine());
+        for (int t = 0; t < K; t++) {
+            int sum = 0;
+            st = new StringTokenizer(br.readLine());
+            int i = Integer.parseInt(st.nextToken());
+            int j = Integer.parseInt(st.nextToken());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+            for (int row = i; row <= x; row++) {
+                for (int col = j; col <= y; col++) {
+                    sum += board[row][col];
+                }
+            }
+            System.out.println(sum);
+        }
+
+    }
+}
